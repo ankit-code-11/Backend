ad66f93fcd78f1dd9ce7a4a46d9f8bfd8146ea76
Ankit Yadav <ankity@silveroakhealth.com>
2022-11-24 10:28:15 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// function bmi(weight, height) {
+//     let bmi = weight / (height * height);
+
+//     if(bmi<= 18.5) return 'Underweight'
+//     else if(bmi <=25.0) return "Normal"
+//     else if(bmi <= 30.0) return 'Overweight';
+//     else if(bmi > 30) return 'Obese'
+
+//   }
+
+  
+
+//   console.log(bmi(80,1.80))
+
+var _ = require('underscore');
+
+let results = _.contains([1,2,3,4], 2);
+
+console.log(results)
